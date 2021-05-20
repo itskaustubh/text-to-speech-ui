1bef15e045601481cb2f8371508a20caa5d0a98e
itskaustubh <kaustubhpb@gmail.com>
2021-05-20 21:03:41 +0530

    second deploy attempt

---
+++ src/components/Translate.svelte
@@ -0,0 +1,29 @@
+<p>{sentence}</p>
+
+<script>
+
+    let sentence = ''
+
+    // translate(`I'm fine.`, {
+    //     to: "Hindi",
+    // }).then((resp) => {
+    //     console.log(resp.data[0])
+    // })
+
+    async function translateText(){
+        // translate('Ik spreek Engels', {to: 'en'}).then(res => {
+        //     console.log(res.text);
+        //     //=> I speak English
+        //     console.log(res.from.language.iso);
+        //     //=> nl
+        // }).catch(err => {
+        //     console.error(err);
+        // }); 
+    }
+
+    translateText()
+</script>
+
+<style>
+
+</style>
