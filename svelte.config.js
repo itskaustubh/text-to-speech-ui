e52a57443002ec7093a63b7e076e49da65524c2f
itskaustubh <56978887+itskaustubh@users.noreply.github.com>
2021-06-22 07:42:33 +0000

    Deploying to gh-pages from @ itskaustubh/text-to-speech-ui@efe8abf3b2d01a5c812a44537c583cbec258caa2 🚀

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-// https://daveceddia.com/svelte-with-sass-in-vscode/
-// svelte.config.js
-const preprocess = require('svelte-preprocess');
-
-module.exports = {
-    preprocess: preprocess(),
-    // ...other svelte options could go here
-};
-
